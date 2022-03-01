cc198e36d8960d948f99cdc3a8413d3b51b4aa29
Ceren Tüker <cerentuker@gmail.com>
2022-03-01 09:27:34 +0100

    added naive min max index search in sorted array

---
+++ Javascript/find-low-high-index-in-sorted-array.js
@@ -0,0 +1,31 @@
+
+/**
+ * 
+ * Given a sorted array of integers, returns the low and high index of the given key. Returns -1 if the indexes are not found.
+ * 
+*/
+
+// naive 
+let findLowIndex = function (arr, key) {
+    for (let i = 0; i <= arr.length; i++) {
+      if (key == arr[i]) {
+        return i;
+      }
+    }
+    return -1;
+  };
+  
+  let findHighIndex = function (arr, key) {
+    let startIndex = findLowIndex(arr, key)
+    if ((arr.length - 1) == startIndex) {
+      return startIndex;
+    }
+    for (let i = startIndex + 1; i<arr.length; i++ ){
+      if (key !== arr[i]) {
+        return i-1;
+      }
+    }
+    return -1;
+  };
+
+// binary search
